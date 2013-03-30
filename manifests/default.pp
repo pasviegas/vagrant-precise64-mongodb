be03e6ffa55f9ce3f3b102440688d1e883f147d0
RamonPage <ramonpage@gmail.com>
2013-03-30 06:00:43 -0300

    Improving recipe.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,31 @@
 class mongodb {
-  package { 'mongodb': ensure => installed }
+  Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+  exec {
+    'apt-update':
+      command => "apt-get update";
+    'apt-key':
+      command => 'apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10',
+      require => Exec['apt-update'];
+    '10gen-dist':
+      command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >> /etc/apt/sources.list",
+      require => Exec['apt-key']
+  }
+
+  package { 'mongodb':
+    ensure => installed,
+    require => Exec['10gen-dist']
+  }
+
   service { 'mongodb':
     ensure  => running,
-    require => Package['mongodb'],
+    require => Package['mongodb']
   }
+
   file { 'mongodb.conf':
     path    => '/etc/mongodb.conf',
     content => template("mongodb/mongodb.conf.erb"),
-    notify  => Service['mongodb'],
+    notify  => Service['mongodb']
   }
 }
 

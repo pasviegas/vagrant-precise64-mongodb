088090cf384c1bdeaec471d2c319d4be8ad749d7
RamonPage <ramonpage@gmail.com>
2013-04-02 03:23:43 -0300

    Adding support to mongodb-10gen package.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ You can use this as a database server for any project.
 2. Install [VirtualBox](http://www.virtualbox.org/)
 3. Install this box on vagrant
 
-        vagrant box add precise64-mongodb https://dl.dropbox.com/s/mpqr1dvzxlva673/precise64-mongodb.box
+        vagrant box add precise64-mongodb [BOX_URL]
 
 4. Init it on your project directory
 
@@ -56,11 +56,6 @@ How to Contribute?
 
 4. Make changes
 5. Open a pull request
-
-TO-DO
------------
-
-* Change mongodb package from `mondodb` to `mongodb-10gen` (puppet recipe).
 
 Maintainer
 ----------
--- manifests/default.pp
+++ manifests/default.pp
@@ -2,24 +2,25 @@ class mongodb {
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
   exec {
-    'apt-update':
-      command => "apt-get update";
     'apt-key':
-      command => 'apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10',
-      require => Exec['apt-update'];
+      command => 'apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10';
     '10gen-dist':
       command => "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >> /etc/apt/sources.list",
-      require => Exec['apt-key']
+      require => Exec['apt-key'];
+    'apt-update':
+      command => "apt-get update",
+      require => Exec['10gen-dist']
   }
 
-  package { 'mongodb':
-    ensure => installed,
-    require => Exec['10gen-dist']
+  package { 'mongodb-10gen':
+    ensure => present,
+    require => Exec['apt-update']
   }
 
   service { 'mongodb':
-    ensure  => running,
-    require => Package['mongodb']
+    enable => true,
+    ensure => running,
+    require => Package['mongodb-10gen']
   }
 
   file { 'mongodb.conf':
